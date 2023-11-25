5070046ac8c246f5cba304b5f4978703fb465583
issamlahrech <147448984+issamlahrech@users.noreply.github.com>
2023-11-25 12:24:17 +0100

    Add files via upload

---
+++ WebApplication3/src/java/test/Test.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import entities.Employe;
+import entities.Service;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author ACER
+ */
+public class Test {
+    public static void main(String[] args) {
+        // Create SessionFactory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Service.class).buildSessionFactory();
+        SessionFactory factory1 = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employe.class).buildSessionFactory();
+        
+
+        // Create Session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // Begin transaction
+            session.beginTransaction();
+
+            // Commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Tables créées avec succès!");
+        } finally {
+            factory.close();
+        }
+    }
+}
+    
+
